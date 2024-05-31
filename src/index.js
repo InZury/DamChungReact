cddef635bcad71bd7883dbbc63decc8da6f276b7
한인철 <ironin0923@gmail.com>
2024-05-31 17:12:38 +0900

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    test.
+    담청 빌드 테스트
   </React.StrictMode>
 );
 
